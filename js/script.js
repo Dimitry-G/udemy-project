edaaf567a322f2b22e3b9e330c36b827aa6e813d
Dimitry <6221011@gmail.com>
2020-08-24 11:38:15 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,98 @@
+// "use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// // console.log ('number', number);
+// // console.log ('leftBorderWidth', leftBorderWidth);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Ivan';
+// console.log(name);
+
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+
+// console.log('string' * 9);
+
+// const person = "Alex";
+
+// const bool = false;
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false,
+// };
+
+// console.log(obj.name);
+// console.log(obj['name']);
+
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp'];
+// console.log(arr[1]);
+
+// alert('Hello');
+
+// const result = confirm ("Are you here?");
+// console.log(result);
+
+// const answer = +prompt('Vam est 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// // document.write(answers);
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// // console.log('https://someurl.com/' + category);
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Dima";
+
+// alert(`Privet, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked || !isClose);
+
